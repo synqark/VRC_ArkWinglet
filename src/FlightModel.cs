6dd86d86292498bf4a6ae48c9b5bc1a54be0943a
synqark <virtually.synqarks@gmail.com>
2022-10-23 13:14:25 +0900

    クラス追加

---
+++ src/FlightModel.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace ArkWinglet
+{
+    /*
+    フライトモデル：
+    文字通り、展開・飛行中の挙動計算を行う
+    コアローカル・コアリモートそれぞれが持つクラスを想定しているが、共通部分があまりにも少なかったら
+    このクラスはなかったことにしてそれぞれのコアクラスで挙動の計算を行う。
+    （ただ、ローカル・リモートに限らずウィングレットの性能や挙動特性を示すパラメーターは同一にしたい...）
+    */
+    public class FlightModel : UdonSharpBehaviour
+    {
+        void Start()
+        {
+            
+        }
+    }
+}
---
+++ src/VisualEffector.cs
@@ -0,0 +1,21 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace ArkWinglet
+{
+    /*
+    視覚エフェクタ：
+    ほぼ全てのコンポーネントを参照・管理し、ウィングレットに関連する適切なイベントを発火する機能を持つ、
+    コンポーネント利用者（ワールド作成者）側で特定のパーティクルを再生させたいときなどに、
+    本クラスが発火したイベントを使ってもらう。
+    */
+    public class VisualEffector : UdonSharpBehaviour
+    {
+        void Start()
+        {
+            
+        }
+    }
+}
